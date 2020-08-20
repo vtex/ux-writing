5358aa413b6fa8179a212405aae1273fd386cd6e
JulianaMeyerArruda <59611647+JulianaMeyerArruda@users.noreply.github.com>
2020-08-20 16:41:50 -0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -32,7 +32,7 @@ function Home() {
       <header className={styles.heroBanner}>
         <div className="container">
           <h1 className={styles.hero__title}>The right words, with the right voice.</h1>
-          <p className={styles.hero__subtitle}>We craft precise microcopy, that transmits our brand’s voice, and achieves our user’s goals. Through this guide,  you will master the art of UX Writing at VTEX.  </p>
+          <p className={styles.hero__subtitle}>We craft precise microcopy, that transmits our brand’s voice, and achieves our user’s goals. Through this guide, you will master the art of UX Writing at VTEX. </p>
           <div className={styles.buttons}>
             <Link
               className={clsx(
